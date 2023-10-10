08829504015f8a294f0afecca4cf7319712d9faa
UnaiPerez <unaiperezmonje@gmail.com>
2023-10-10 18:25:10 +0200

    Agregar archivos de proyecto.

---
+++ Common/MergeSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class MergeSort : ISortingAlgorithm
+    {
+        public int[] Partition(int[] A, int startIndex, int endIndex)
+        {
+            //TODO #1: return a new array with all elements in A from index startIndex to endIndex (both included): A[startIndex..endIndex]
+
+            int tamañoPartition = endIndex - startIndex + 1;
+            int[] partition = new int[tamañoPartition];
+            for (int i = 0; i < tamañoPartition;i++) 
+            {
+                partition[i] = A[startIndex + i];
+            }
+     
+            return partition;
+        }
+
+        public void MergePartitions(int[] A, int[] leftPartition, int[] rightPartition)
+        {
+            //TODO #2: Merge in A the two partitions sorting them
+            
+        }
+
+        public void Sort(int[] A)
+        {
+            //TODO #3: Implement MergeSort using the methods above
+        }
+
+        public bool CheckIsCorrect()
+        {
+            int n = 10;
+            int[] A = Utils.CreateIntArray(n);
+
+            Console.WriteLine("1.1 Checking Partition()");
+            if (!Utils.IsPartitionCorrect(A, Partition(A, 0, 9), 0, 3))
+            {
+                Console.WriteLine("FAILED");
+                return false;
+            }
+            if (!Utils.IsPartitionCorrect(A, Partition(A, 1, 1), 1, 1))
+            {
+                Console.WriteLine("FAILED");
+                return false;
+            }
+            Console.WriteLine("PASSED");
+            Console.WriteLine("1.2. Checking MergePartitions()");
+            int[] leftPartition = new int[3] { 1, 4, 6 };
+            int[] rightPartition = new int[3] { 2, 3, 7 };
+            int[] merged = new int[6] { 1, 4, 6, 2, 3, 7 };
+            MergePartitions(merged, leftPartition, rightPartition);
+            if (!Utils.IsOrdered(merged))
+            {
+                Console.WriteLine("FAILED");
+                return false;
+            }
+            Console.WriteLine("PASSED");
+            return true;
+        }
+    }
+}
---
+++ Common/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class SelectionSort : ISortingAlgorithm
+    {
+        
+
+        public void Sort(int[] A)
+        {
+            //TODO #1: Implement SelectionSort
+            for (int pivot = 0;pivot < A.Length;pivot++) {
+                for (int j = pivot + 1;j < A.Length;j++) {
+                    if (A[j] < A[pivot])
+                    {
+                        Swap(A,pivot,j);
+                    }
+                }
+            }
+            
+        }
+
+        private void Swap(int[] A, int pivot, int j)
+        {
+            int num = A[pivot];
+            A[pivot] = A[j];
+            A[j] = num;
+        }
+
+        public bool CheckIsCorrect()
+        {
+            int n = 10;
+            int[] A = Utils.CreateIntArray(n);
+
+            Console.WriteLine("1.1 Checking Sort()");
+            Sort(A);
+            if (!Utils.IsOrdered(A))
+            {
+                Console.WriteLine("FAILED");
+                return false;
+            }
+            Console.WriteLine("PASSED");
+            return true;
+        }
+    }
+}
